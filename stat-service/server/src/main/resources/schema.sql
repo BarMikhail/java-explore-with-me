fc4096098c8b122ace9d3334a2e21a3b55ff32c4
М.Баранов <m.baranov@rlt.ru>
2024-04-21 21:21:58 +0300

    Публичный проект

--- stat-service/server/src/main/resources/schema.sql
+++ stat-service/server/src/main/resources/schema.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS stat(
     app         VARCHAR(50)     NOT NULL,
     uri         VARCHAR(50)     NOT NULL,
     ip          VARCHAR(15)     NOT NULL,
-    timestamp   TIMESTAMP       WITHOUT TIME ZONE
+    timestamp   TIMESTAMP       WITHOUT TIME ZONE NOT NULL
 );
